3f905e2f9d741bc67c57818659fc465aee9b3cab
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-23 09:33:02 -0300

    feat: adds superuser creation in the start script

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 python manage.py makemigrations
 
 python manage.py migrate
+
+python manage.py seed_db --create-super-user
 
 python manage.py runserver "$DJANGO_DEV_HOST":"$DJANGO_DEV_PORT"
